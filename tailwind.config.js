f59a58da388fa8750f93565034ba8c4d81c1c397
Jamey <jameyluxx@gmail.com>
2025-12-01 10:25:03 +0800

    update intake page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,25 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,jsx,ts,tsx,mdx}",
+    "./components/**/*.{js,jsx,ts,tsx,mdx}",
+    "./lib/**/*.{js,jsx,ts,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: "1.5rem",
+        screens: {
+          "2xl": "1280px",
+        },
+      },
+      borderRadius: {
+        lg: "0.5rem",
+        xl: "0.75rem",
+        "2xl": "1rem",
+      },
+    },
   },
   plugins: [],
 };
